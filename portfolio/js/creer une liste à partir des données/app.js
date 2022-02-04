58e17d359be3dcd09b8ef4950d6b75faabe5aacc
raphael56 <raphael.hervieux@gmail.com>
2022-02-04 08:39:05 +0100

    cours JS

---
+++ portfolio/js/creer une liste à partir des données/app.js
@@ -0,0 +1,31 @@
+
+// pour aller chercher dans une API
+
+// const liste = document.querySelector('.liste');
+// const btn = document.querySelector('.btn');
+// const xhr = new XMLHttpRequest();
+
+// btn.addEventListener('click', () => {
+   
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(response => response.json())
+//     .then(data => {
+//         for (i=0; i < data.length; i++) {
+
+//             let newLi = document.createElement('li');
+//             let newTitreCarte= document.createElement('h2');
+//             let newBodyCarte = document.createElement('p');
+        
+//             newTitreCarte.innerText = data[i].title;
+//             newBodyCarte.innerText = data[i].body;
+
+//             newLi.appendChild(newTitreCarte);
+//             newLi.appendChild(newBodyCarte);
+
+//             liste.appendChild(newLi);
+//         }
+//     }
+//     )
+// })
+
+
